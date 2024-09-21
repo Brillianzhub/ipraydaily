b4596b8710c63fddee258a956360d17131de1132
Brillianzhub <brillianzhub@gmail.com>
2024-09-21 15:17:41 +0200

    Created components folder

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <>
+            <h2>About IPray Daily</h2>
+            <p>We are Christ ambassadors. Created in the image of God to the praises of God who has called us in to his marvellou light. We are serving God through
+                creation of scripture based prayers to help believers. We also provide Bible resources in various versions to help understanding. Other services are provision
+                of Bible API and raw Bible Data in different formats to aid in spreading the good news across the world!</p>
+        </>
+    )
+}
+
+export default About;
