1de961c5210ae6b95d666c0793baf4c1fb33b237
Brillianzhub <brillianzhub@gmail.com>
2024-10-28 17:50:20 +0100

    Add meta tags for seo

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import ReactDom from "react-dom/client";
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-// import { BibleDataProvider } from './context/BibleDataContext';
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <BibleDataProvider> */}
       <App />
-      {/* </BibleDataProvider> */}
     </BrowserRouter>
   </React.StrictMode>
 )
